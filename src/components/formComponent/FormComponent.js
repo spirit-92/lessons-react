cf59c6fc92135a383b4ae1ed37b4bbfb4ed5d476
spirit-92 <panasenkodima97@gmail.com>
2022-10-13 02:19:14 +0300

    form

--- src/components/formComponent/FormComponent.js
+++ src/components/formComponent/FormComponent.js
@@ -1,24 +1,33 @@
 import './FormStyle.css'
+import {useState} from "react";
 
 function FormComponent() {
+
+    const [stateData,changeData] = useState({
+        name:'',
+        password:''
+    })
     function submitForm(e){
         e.preventDefault()
-        const saveDate = {
-            name:e.target.name.value,
-            password:e.target.password.value
-        }
-        console.log(saveDate)
+
+        console.log(stateData)
     }
+    function changeStateF(text , name){
+        changeData({...stateData , [name]:text})
+    }
+
+
+
     return (
         <>
             <div className="container__form">
                 <h3 style={{textAlign:'center'}}>Form login</h3>
                 <form onSubmit={submitForm}>
-                    <label htmlFor="fname">First Name</label>
-                    <input type="text" id="fname" name="name" placeholder="Your name.." />
+                    <label htmlFor="name_input">First Name</label>
+                    <input type="text" id="name_input" value={stateData.name} onChange={(e)=>changeStateF(e.target.value,'name')} placeholder="Your name.." />
 
-                        <label htmlFor="lname">Last Name</label>
-                        <input type="password" id="lname" name="password" placeholder="Your last name.." />
+                        <label htmlFor="password_input">Last Name</label>
+                        <input type="password" id="password_input" value={stateData.password} onChange={(e)=>changeStateF(e.target.value,'password')}  placeholder="Your last name.." />
                         <input type="submit" value="Submit" />
                 </form>
             </div>
