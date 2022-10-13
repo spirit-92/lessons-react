92b5667a2193124a236b6094d6ca187019398e84
spirit-92 <panasenkodima97@gmail.com>
2022-10-13 21:58:19 +0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  const [todo,setTodo] = useState(null)
+
+    useEffect( ()=>{
+        console.log('laceholder')
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+            .then(response => response.json())
+            .then(json => setTodo(json))
+    },[])
+
   return (
     <div className="App">
-      <h1>test</h1>
+      <h1>{todo?todo.title:'loader '}</h1>
     </div>
   );
 }
